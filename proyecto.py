7315a7018aeae9a5a9127f7b13aa0d1da4b12075
proGONmacion <111333110+proGONmacion@users.noreply.github.com>
2022-10-14 05:02:30 -0500

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,15 @@
+datos = ["semieje mayor", "semieje menor", "distancia focal", "excentricidad",
+         "afelio", "perihelio", "periodo orbital",
+         "masa del cuerpo que es el foco de la orbita"]
+for d in datos:
+    print(datos.index(d) + 1 , d)
+    
+instruccion = "seleccione los datos que dispone"
+explicacion = "introduciendo el número a la izquierda de estos"
+print(instruccion, explicacion)
+
+opcion = input()
+
+
+
+
